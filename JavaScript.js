c911a948b5e4686029ca477449ece2caddd15e7c
Sabrina Novaes <sabrinanovaes96@gmail.com>
2022-09-10 21:51:23 -0300

    Correção

--- JavaScript.js
+++ JavaScript.js
@@ -1,21 +1,3 @@
-function resposta(){
+function resposta() {
     alert("Comentário enviado");
-}
-
-function validar(){
-    var nome = document.getElementById("nome");
-
-    if (nome(nome.value == "")){
-        alert("Login inválido");
-        nome.focus();
-    }
-}
-
-function validar(){
-    var senha = document.getElementById("senha");
-
-    if (senha(senha.value == "")){
-        alert("Senha inválida");
-        senha.focus();
-    }
 }
